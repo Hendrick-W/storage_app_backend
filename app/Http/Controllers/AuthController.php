d16a14dae07699950d04ef9128fe1a1dd96ed1b3
Hendrick <hendrickwijaya21@gmail.com>
2021-04-02 00:01:09 +0700

    Changing return response key in get user info

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -115,7 +115,7 @@ class AuthController extends Controller
         }
         return response()->json([
             "status" => true,
-            "message" => $user,
+            "user" => $user,
         ]);
     }
     public function getalluser(Request $request)
